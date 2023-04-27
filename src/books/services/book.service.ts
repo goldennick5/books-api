6af5e3d8cba29b8759c7825ce58329a7b6687591
tokhtaraubakirov <student.aubakirov@dku.kz>
2023-04-27 23:58:00 +0600

    adding methods to BookService

--- src/books/services/book.service.ts
+++ src/books/services/book.service.ts
@@ -1,8 +1,24 @@
-import Books from "../models/book.model";
+import Books, { BooksInput, BooksOutput } from '../models/book.model'
 
 class BookService {
-  async getAllBooks(): Promise<Books[]> {
-    return await Books.findAll();
+  // indicating that the id property is optional by giving to book parameter BooksInput interface
+  async createBook (book: BooksInput): Promise<Books> {
+    return await Books.create(book)
+  }
+
+  async getAllBooks (): Promise<Books[]> {
+    return await Books.findAll()
+  }
+  
+  // ensuring that the id property is always present in the returned object
+  async getOneBook (id: number): Promise<BooksOutput | null> {
+    return await Books.findByPk(id)
+  }
+
+  async updateBook (id: number, book: Books): Promise<number> {
+    // using destructuring, we can assign the first element of the array to a variable and ignore the second element [number, Books[]]
+    const [numAffectedRows] = await Books.update(book, { where: { id } })
+    return numAffectedRows
   }
 }
 
